16a65546f235b12ab36f4de4f2999007bab4605a
higab85 <gabrielhirschbaeck@gmail.com>
2017-09-27 13:52:36 +0200

    working on android studio 2.3

---
+++ app/src/main/java/plusone/plusone/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package plusone.plusone
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        // Set up the login form.
+
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
+
+
+
