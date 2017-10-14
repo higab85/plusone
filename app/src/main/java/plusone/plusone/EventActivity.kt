c2c9c1ad0c5421ab4dbc8793f391eface3589fe8
Unknown <ja.merino@usp.ceu.es>
2017-10-14 12:05:54 +0200

    EventActivity completa pero falta expandableList

---
+++ app/src/main/java/plusone/plusone/EventActivity.kt
@@ -0,0 +1,12 @@
+package plusone.plusone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityEvent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event2)
+    }
+}
