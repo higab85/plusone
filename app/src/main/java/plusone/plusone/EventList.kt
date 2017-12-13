1518c7467fa8bb69840aa6dc3e4a4984f115afd6
Unknown <allancerol@gmail.com>
2017-12-13 20:59:28 +0100

    Localisation of cellphone DONE

--- app/src/main/java/plusone/plusone/EventList.kt
+++ app/src/main/java/plusone/plusone/EventList.kt
@@ -1,5 +1,6 @@
 package plusone.plusone
 
+import android.Manifest
 import android.app.DatePickerDialog
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
@@ -14,11 +15,22 @@ import android.view.View
 import android.widget.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
+import plusone.plusone.R.id.editTextSearch2
 
 
 // This activity will show an undetailed list of all the events (filtered or not depending on
 // whether a filter has been applied or not).
-class EventList : AppCompatActivity() {
+class EventList : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+
     private var eventsInfoList:List<Event>? = null
     private var eventsInfoListCurated:List<Event>? = null
 
@@ -26,13 +38,22 @@ class EventList : AppCompatActivity() {
     var eventListInfoAdapter: EventListInfoAdapter? = null
     val searchWord:String = ""
 
+    val PERMISSON_REQUEST_CODE = 1001
+    val PLAY_SERVICE_RESOLUTION_REQUEST = 1000
+    var mGoogleApiClient: GoogleApiClient? = null
+    var mLocationRequest: LocationRequest? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_list)
+
+        requestPermission()
+        if(checkPlayService()){
+            buildGoogleApiCLient()
+        }
+
         val searchHome = this.intent.getStringExtra("searchHome")
-
-
         if (searchHome==""){
             refreshEventData().execute()
         }
@@ -85,7 +106,103 @@ class EventList : AppCompatActivity() {
                 myRecyclerView?.adapter= eventListInfoAdapter
             }
         }
+
+
     }
+
+    private fun requestPermission() {
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSON_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode){
+            PERMISSON_REQUEST_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(checkPlayService()){
+                        buildGoogleApiCLient()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun buildGoogleApiCLient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build()
+    }
+
+    private fun checkPlayService(): Boolean {
+
+        val resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+        if(resultCode != ConnectionResult.SUCCESS){
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICE_RESOLUTION_REQUEST).show()
+            }
+            else{
+                Toast.makeText(applicationContext,"This device is not supported", Toast.LENGTH_LONG).show()
+                finish()
+            }
+            return false
+        }
+        return true
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        createLocationRequest();
+    }
+
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 10000
+        mLocationRequest!!.fastestInterval = 5000
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this)
+    }
+
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.i("ERROR", "Connection Failed :"+p0.errorCode)
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+        Toast.makeText(applicationContext,"${p0!!.latitude} - ${p0!!.longitude}", Toast.LENGTH_LONG).show()
+        //editTextSearch2. = "${p0!!.latitude} - ${p0!!.longitude}"
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+       mGoogleApiClient!!.connect()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(mGoogleApiClient!=null){
+            mGoogleApiClient!!.connect()
+        }
+    }
+
+    override fun onDestroy() {
+        mGoogleApiClient!!.disconnect()
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPlayService()
+    }
+
 
     fun funTime(view: View, date:String){
         val c = Calendar.getInstance()
@@ -209,4 +326,9 @@ class EventList : AppCompatActivity() {
         }
     }
 
-}
+
+
+
+    ///Code to take the location of the device
+
+   }
